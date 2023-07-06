7bdf02cf6f9fe02bd2065c5098b6877bc2e47cb7
Scott L <ehee27@gmail.com>
2023-07-06 07:28:27 -0500

    CORS backend update with deploy

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
-  'http://localhost:3000',
+  'http://localhost:3000/',
   'https://scottnotes.onrender.com/',
+  'https://scottnotes-api.onrender.com/',
 ]
 
 module.exports = allowedOrigins
