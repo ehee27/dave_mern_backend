c7f4dd6f67beac5db023718c3be677a859dbefe5
Scott L <ehee27@gmail.com>
2023-07-07 08:26:39 -0500

    updated routing2

--- server.js
+++ server.js
@@ -28,9 +28,7 @@ app.use('/', require('./routes/root'))
 app.use('/auth', require('./routes/authRoutes'))
 app.use('/users', require('./routes/userRoutes'))
 app.use('/notes', require('./routes/noteRoutes'))
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, 'build', 'index.html'))
-)
+
 //
 app.all('*', (req, res) => {
   res.status(404)
